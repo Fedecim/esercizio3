01c03f7312b6f71b9e771e2636b7d4a00eca4c93
fedecim <federico.cimatti93@gmail.com>
2023-10-19 21:22:54 +0200

    impementato modulo processa_form per funzionalita' registrati

--- private/processa_form.php
+++ private/processa_form.php
@@ -1,3 +1,16 @@
 <?php
+session_start();
+require "/opt/lampp/htdocs/esercizio3/private/controller/regController.php";
 
+if(isset($_POST["registrati"])){
+    $dati = array(
+        "username"=>$_POST["username"],
+        "email"=>$_POST["email"],
+        "password"=>$_POST["password"],
+        "ruolo"=>$_POST["ruolo"],
+        "chiave"=>$_POST["chiave"]
+    );
+    $reg = new regController();
+    $reg->add_utente($dati);
+}
 ?>
